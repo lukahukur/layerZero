a878208ac0b3e394a4b62a9b69cfb1f4ffa230b3
lukahukur <6wingman9@gmail.com>
2023-04-27 13:47:34 +0400

    showcase

--- src/components/showcase/Showcase.tsx
+++ src/components/showcase/Showcase.tsx
@@ -1,4 +1,5 @@
 import styles from './styles.module.scss'
+import { Link } from 'react-router-dom'
 const ShowCase = () => {
   return (
     <section className={styles.section}>
@@ -6,19 +7,35 @@ const ShowCase = () => {
       <div>
         <ul>
           <li>
-            <div>01 / </div>
-            <p>Buybot GIF</p>
+            <span>
+              <div>01 / </div>
+              <p>Buybot GIF</p>
+            </span>
+
+            <section>Test</section>
           </li>
           <li>
-            <div>02 / </div>
-            <p>Video creation</p>
+            <span>
+              <div>02 / </div>
+              <p>Video creation</p>
+            </span>
+
+            <section>Test</section>
           </li>
           <li>
-            <div>03 / </div>
-            <p>Logo creation</p>
+            <span>
+              <div>03 / </div>
+              <p>Logo creation</p>
+            </span>
+
+            <section>Test</section>
           </li>
         </ul>
-        <div id={'community'}>DEVELOP WITH DOGEGPT</div>
+        <div>
+          <Link to={'/'} className={'underline_link_black'} id={'community'}>
+            DEVELOP WITH DOGEGPT
+          </Link>
+        </div>
       </div>
     </section>
   )
